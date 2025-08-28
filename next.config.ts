0a8406365983d40f494f8b7993910d799b360504
TomHung <tomworkpurpose@gmail.com>
2025-08-28 15:38:45 +0800

    deploy test setting

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* deploy test */
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
