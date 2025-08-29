535c9f96e2737867f134e2c0b4a0e38025ff901e
TomHung <tomworkpurpose@gmail.com>
2025-08-29 12:39:23 +0800

    add vapi interview generate

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,26 +1,16 @@
 import React from "react";
-import Image from "next/image";
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Interview = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
-    <div className="flex flex-col gap-6 mt-8">
-      {/* title */}
-      <div className="flex justify-between">
-        <div className="flex gap-2 items-center">
-          <Image
-            className="rounded-full object-fit size-[30px]"
-            src="/covers/adobe.png"
-            alt="ai"
-            width={30}
-            height={30}
-          />
-          <h2>Frontend Developer Interview</h2>
-        </div>
-      </div>
-      <Agent userName="test" type="generate" />
-    </div>
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName={user?.name!} userId={user?.id} type="generate" />
+    </>
   );
 };
 
-export default Interview;
+export default Page;
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { vapi } from "@/lib/vapi.sdk";
+import { useRouter } from "next/navigation";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -8,20 +12,109 @@ enum CallStatus {
   FINISHED = "FINISHED",
 }
 
-const Agent = ({ userName = "test" }: AgentProps) => {
-  const isSpeaking = true;
-  const callStatus = CallStatus.ACTIVE;
-  const messages = ["What yout name", "What yout name", "What yout name"];
-  const lastMessage = messages[messages.length - 1];
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
+
+const Agent = ({ userName, userId, type }: AgentProps) => {
+  const router = useRouter();
+
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [lastMessage, setLastMessage] = useState("");
+
+  // 初始化 vapi
+  useEffect(() => {
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
+
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
+
+    const onSpeechStart = () => {
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => {
+      setIsSpeaking(false);
+    };
+
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        setMessages((prev) => [
+          ...prev,
+          {
+            role: message.role,
+            content: message.transcript,
+          },
+        ]);
+      }
+    };
+
+    const onError = (error: Error) => {
+      console.log("Error", error);
+    };
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+    vapi.on("message", onMessage);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+      vapi.off("message", onMessage);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLastMessage(messages[messages.length - 1].content);
+    }
+
+    if (callStatus === "FINISHED") {
+      router.push("/");
+    }
+  }, [callStatus, messages]);
+
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
+
+    await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID, {
+      variableValues: {
+        username: userName,
+        userid: userId,
+      },
+    });
+  };
+
+  const handleDisconnect = () => {
+    vapi.stop();
+  };
+
+  const isInactiveOrFinished =
+    callStatus === "INACTIVE" || callStatus === "FINISHED" ? true : false;
 
   return (
     <>
       {/* Voice  */}
       <div className="flex flex-wrap gap-3 max-md:flex-col">
         {/* AI */}
-        <div className="flex-1 border-1 rounded-2xl border-fuchsia-200 h-96 flex flex-col justify-center items-center gap-3 max-md:py-15">
+        <div
+          className={`avatar flex-1 border-2 rounded-2xl h-96 flex flex-col justify-center items-center gap-3 max-md:py-15 blue-gradient-dark
+    ${isSpeaking ? "border-fuchsia-200 animate-pulse" : "border-transparent"}`}
+        >
           <Image
-            className="rounded-full object-fit size-[150px]"
+            className="rounded-full object-cover w-[150px] h-[150px]"
             src="/covers/adobe.png"
             alt="ai"
             width={540}
@@ -31,7 +124,7 @@ const Agent = ({ userName = "test" }: AgentProps) => {
         </div>
 
         {/* Interviewer */}
-        <div className="flex-1 border-1 rounded-2xl border-fuchsia-200 h-96 flex flex-col justify-center items-center gap-3 max-md:hidden">
+        <div className="avatar flex-1 border-1 rounded-2xl h-96 flex flex-col justify-center items-center gap-3 max-md:py-15 blue-gradient-dark">
           <Image
             className="rounded-full object-fit size-[150px]"
             src="/user-avatar.png"
@@ -53,15 +146,11 @@ const Agent = ({ userName = "test" }: AgentProps) => {
       {/* Buttons */}
       <div className="title m-auto  space-x-4">
         {callStatus !== "ACTIVE" ? (
-          <button className="btn-call">
-            <span>
-              {callStatus === "INACTIVE" || callStatus === "FINISHED"
-                ? "Call"
-                : "..."}
-            </span>
+          <button className="btn-call" onClick={handleCall}>
+            <span>{isInactiveOrFinished ? "Call" : "..."}</span>
           </button>
         ) : (
-          <button className="btn-disconnect">
+          <button className="btn-disconnect" onClick={handleDisconnect}>
             <span>End</span>
           </button>
         )}
